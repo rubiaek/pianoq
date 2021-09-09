709d41856a0523cc4255df4db20ad9cd74c8df19
Ronen Shekel <ronen.shekel@gmail.com>
2021-09-09 17:41:56 +0300

    better piano_popoff_simulation.py

--- pianoq/simulations/piano_popoff_simulation.py
+++ pianoq/simulations/piano_popoff_simulation.py
@@ -1,6 +1,7 @@
 import time
 from functools import reduce
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from pianoq.lab.optimizations.my_pso import MyPSOOptimizer
@@ -8,8 +9,9 @@ from pianoq.results import PopoffPRXResult
 
 
 class PianoPopoffSimulation(object):
-    def __init__(self, piezo_num=30):
+    def __init__(self, piezo_num=30, normalize_cost_to_tot_power=True):
         self.piezo_num = piezo_num
+        self.normalize_cost_to_tot_power = normalize_cost_to_tot_power
 
         self.pop = PopoffPRXResult(path=PopoffPRXResult.DEFAULT_PATH)
 
@@ -21,19 +23,20 @@ class PianoPopoffSimulation(object):
 
         # In original the elements are ~10^-6, so after 30 TMS we get to really small...
         # self.TMs = [0.3*TM / np.abs(TM.diagonal()).mean() for TM in self.TMs]
-        self.TMs = [TM / 2e-3 for TM in self.TMs]
+        # self.TMs = [TM / 2e-3 for TM in self.TMs]
+        self.TMs = [T / np.sqrt(np.mean(np.sum(np.abs(T) ** 2, 0))) for T in self.TMs]
 
         self.TM_fiber = self.TMs[20]
 
         self.optimizer = None
         self.n_pop = None
-        self.in_modes = np.ones(self.pop.Nmodes)
+        self.in_modes = (1/np.sqrt(self.pop.Nmodes)) * np.ones(self.pop.Nmodes)
 
         self.amps_history = []
 
 
-    def run(self, n_pop=30, n_iterations=30, cost_function=None,
-            stop_after_n_const_iters=10, reduce_at_iterations=(2, 5)):
+    def run(self, n_pop=30, n_iterations=50, cost_function=None,
+            stop_after_n_const_iters=20, reduce_at_iterations=(2, 5)):
 
         cost_function = cost_function or self.cost_function_focus
         self.n_pop = n_pop
@@ -70,28 +73,52 @@ class PianoPopoffSimulation(object):
 
         return pix1, pix2
 
+    def _power_at_area(self, pix1):
+        Nx, Ny = pix1.shape
+        window_size = 2
+        roi = np.index_exp[(Nx // 2) - window_size: (Nx // 2) + window_size,
+                           (Ny // 2) - window_size: (Ny // 2) + window_size]
+        powers = np.abs(pix1[roi]) ** 2
+        return powers.mean()
 
     def cost_function_focus(self, amps):
         """ amps are between 0 and 1 """
 
         pix1, pix2 = self.get_pixels(amps)
 
-        tot_power = (np.abs(pix1) ** 2).sum() + (np.abs(pix2) ** 2).sum()
 
         # (5) "measure" the intensity in some point
-        Nx, Ny = pix1.shape
-        window_size = 2
-        roi = np.index_exp[(Nx // 2) - window_size: (Nx // 2) + window_size,
-                           (Ny // 2) - window_size: (Ny // 2) + window_size]
-        powers = np.abs(pix1[roi]) ** 2
-        return -powers.mean()
-
+        if self.normalize_cost_to_tot_power:
+            tot_power = (np.abs(pix1) ** 2).sum() + (np.abs(pix2) ** 2).sum()
+            cost = -self._power_at_area(pix1) / tot_power
+        else:
+            cost = -self._power_at_area(pix1)
+        return cost
 
     def post_iteration_callback(self, global_best_cost, global_best_positions):
-        print(f'{self.optimizer.curr_iteration}.\t cost: {global_best_cost}\t time: {(time.time()-self.optimizer.start_time):2f} seconds')
+        print(f'{self.optimizer.curr_iteration}.\t cost: {global_best_cost:2f}\t time: {(time.time()-self.optimizer.start_time):2f} seconds')
         self.amps_history.append(global_best_positions)
+
+    def show_before_after(self):
+        fig, axes = plt.subplots(2, 1, figsize=(5, 5.8), constrained_layout=True)
+
+        pix1_0, pix2_0 = self.get_pixels(self.amps_history[0])
+        pix1_1, pix2_1 = self.get_pixels(self.amps_history[-1])
+        pixs0 = np.concatenate((pix1_0, pix2_0), axis=1)
+        pixs1 = np.concatenate((pix1_1, pix2_1), axis=1)
+
+        im0 = axes[0].imshow(np.abs(pixs0)**2)
+        im1 = axes[1].imshow(np.abs(pixs1)**2)
+        fig.colorbar(im0, ax=axes[0])
+        fig.colorbar(im1, ax=axes[1])
+        axes[0].set_title('Before')
+        axes[1].set_title('After')
+
+        fig.show()
 
 
 if __name__ == "__main__":
-    piano_sim = PianoPopoffSimulation()
-    piano_sim.run()
+    piano_sim = PianoPopoffSimulation(piezo_num=30)
+    piano_sim.run(n_pop=30, n_iterations=50)
+    piano_sim.show_before_after()
+    plt.show()
