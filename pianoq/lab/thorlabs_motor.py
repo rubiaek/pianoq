3b51dfb8958d8638a2dccfa715b63003c3b452fd
Shachar Resisi <shachar.resisi@mail.huji.ac.il>
2021-09-12 14:50:43 +0300

    added normal Thorlabs motor class

--- pianoq/lab/thorlabs_motor.py
+++ pianoq/lab/thorlabs_motor.py
@@ -10,73 +10,37 @@ except ImportError:
     print('cant use py_thorlabs_ctrl.kinesis')
 
 
-SERIAL_1 = 27253522
-
-# Has to do with the screwing angle of the waveplate to the inside of the motor
-MY_HWP_ZERO = 3.5505
-
-def get_motor(serial=SERIAL_1):
-    # TODO: make a class that inherits KCubeDCServo so you can have the .close() like you like, and always cast to float
-    motor = KCubeDCServo(serial)
-    motor.create()
-    motor.enable()
-    # To finish use motor.disable() and then motor.disconnect
-    # when using "move_absolute" or "move_relative" always use "float"
-    # movement units are typically in mm, but in our case they are in degrees (0-360)
-    return motor
-
-class ThorLabsMotor(object):
+class ThorlabsRotatingServoMotor(KCubeDCServo):
     """
     Uses this library to communicate with the motors:
-    https://github.com/rwalle/py_thorlabs_ctrl
-    Note: I (Alon) changed a little bit the library itself
+        https://github.com/rwalle/py_thorlabs_ctrl
+    Note: Alon changed a little bit the library itself to support another type of KCube or something,
+    and also tried to add timeout
 
-    All units here are in mm
     """
 
-    def __init__(self):
-        py_thorlabs_ctrl.kinesis.init(self._KINESIS_PATH)
-        from py_thorlabs_ctrl.kinesis.motor import KCubeDCServo
-        from py_thorlabs_ctrl.kinesis.motor import KCubeStepper
-        self._KCubeDCServo = KCubeDCServo
+    SERIAL_1 = 27253522
 
-        self.serial = 27253522
-        self._motor = self._create_motor(26001271, KCubeStepper)
+    # Has to do with the screwing angle of the waveplate to the inside of the motor
+    MY_HWP_ZERO = 3.5505
 
-        self._moving_timeout = 20 * 1000  # In milliseconds
+    def __init__(self, serial_number=None, zero_angle=None):
+        self.zero_angle = zero_angle or self.MY_HWP_ZERO
+        serial_number = serial_number or self.SERIAL_1
+        super().__init__(serial_number=serial_number)
+        motor.create()
+        motor.enable()
 
-    def _create_motor(self, serial_number, motor_class):
-        kcube = motor_class(serial_number)
-        kcube.create()
-        kcube.enable()
-        return kcube
+    def move_relative(self, degrees):
+        # movement units are typically in mm, but in our case they are in degrees (0-360)
+        # It is important to send float, and not some other np type...
+        super().move_relative(float(degrees))
 
-    def move_relative(self, motor, mms):
-        """move in mm units"""
-        motor.move_relative(float(mms), self._moving_timeout)
-
-    def move_absolute(self, motor, location):
-        """Location in mms"""
-        # Note: location may be float or numpy.float64, but if it is numpy.float64, the motor doesn't move to the
-        # right place. For example, moving to 12.5 will result in moving to 12.0. This is why we have to convert
-        # the type here
-        motor.move_absolute(float(location), self._moving_timeout)
-
-    def move_x_relative(self, mms):
-        self.move_relative(self.x_motor, mms)
-
-    def move_y_relative(self, mms):
-        self.move_relative(self.y_motor, mms)
-
-    def move_x_absolute(self, location):
-        self.move_absolute(self.x_motor, location)
-
-    def move_y_absolute(self, location):
-        self.move_absolute(self.y_motor, location)
-
-    def move_z_absolute(self, location):
-        self.move_absolute(self.z_motor, location)
+    def move_absolute(self, degrees):
+        # movement units are typically in mm, but in our case they are in degrees (0-360)
+        # It is important to send float, and not some other np type...
+        super().move_absolute(float(degrees + self.zero_angle))
 
     def close(self):
-        for motor in [self.x_motor, self.y_motor, self.z_motor]:
-            motor.disconnect()
+        self.disable()
+        self.disconnect()
