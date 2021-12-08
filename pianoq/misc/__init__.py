dcc408be8aa7ee896c6d4b34f6aac693766bcc26
Shachar Resisi <shachar.resisi@mail.huji.ac.il>
2021-12-08 13:02:48 +0200

    live_cam

--- pianoq/misc/__init__.py
+++ pianoq/misc/__init__.py
@@ -1,3 +1,11 @@
+import traceback
 from pianoq.misc.borders import Borders
 from pianoq.misc.consts import DEFAULT_BORDERS
 from pianoq.misc.mplt import mplot, mimshow
+
+try:
+    from pianoq.lab.scripts.live_camera import live_cam
+except Exception as e:
+    print("ERROR!!")
+    print(e)
+    traceback.print_exc()
