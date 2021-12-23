6f4c8b447da9f317299098724d8805b8e030f292
Shachar Resisi <shachar.resisi@mail.huji.ac.il>
2021-12-23 14:43:08 +0200

    bgean global optimization with two motors.

---
+++ pianoq/lab/scripts/optimize_global_rotation.py
@@ -0,0 +1,31 @@
+from pianoq.lab import VimbaCamera
+from pianoq.lab.elliptec_stage import ElliptecMotor
+from pianoq.lab.thorlabs_motor import ThorlabsRotatingServoMotor
+from pianoq.misc.consts import DEFAULT_CAM_NO
+
+
+class GlobalRotationOptimization(object):
+    def __init__(self):
+        self.hwm = ThorlabsRotatingServoMotor()
+        self.qwm = ElliptecMotor()
+        self.cam = VimbaCamera(DEFAULT_CAM_NO)
+
+    def run(self):
+        # Run over angles with both WPs and find best position for self.cost_function
+        pass
+
+    def cost_function_H(self):
+        # get the most light into H
+        pass
+
+    def close(self):
+        self.hwm.close()
+        self.qwm.close()
+
+
+if __name__ == "__main__":
+    gro = GlobalRotationOptimization()
+    gro.run()
+    gro.close()
+
+
