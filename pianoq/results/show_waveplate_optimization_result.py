042e2b4d134d9a0f2ff6ce8bf285cdf011b014f4
Shachar Resisi <shachar.resisi@mail.huji.ac.il>
2022-02-23 11:35:02 +0200

    Better global optimization result

--- pianoq/results/show_waveplate_optimization_result.py
+++ pianoq/results/show_waveplate_optimization_result.py
@@ -7,6 +7,10 @@ path = sys.argv[1]
 name = os.path.basename(path)
 
 wr = WavePlateOptimizationResult(path)
-wr.show_heatmap()
+# wr.show_heatmap()
+# wr.show_cross_section()
+wr.show_heatmap_with_cross_section()
+print(f"min: {wr.heatmap.min()}")
+print(f"max: {wr.heatmap.max()}")
 
 plt.show()
--- pianoq/results/waveplates_optimization_result.py
+++ pianoq/results/waveplates_optimization_result.py
@@ -1,5 +1,7 @@
 import pickle
 import datetime
+
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -36,3 +38,36 @@ class WavePlateOptimizationResult(object):
         ax.set_ylabel(r'HWP angle')
         fig.colorbar(im, ax=ax)
         fig.show()
+
+    def show_cross_section(self):
+        fig, ax = plt.subplots()
+        ax.set_title("Cross section with most visibility")
+        I = self.heatmap.argmax()
+        i, j = np.unravel_index(I, self.heatmap.shape)
+
+        cross_section = self.heatmap[:, j]
+        ax.plot(self.H_angles, cross_section)
+        ax.set_xlabel(r'HWP angle')
+        ax.set_ylabel(r'intensity in H')
+        fig.show()
+
+    def show_heatmap_with_cross_section(self):
+        fig, axes = plt.subplots(1, 2, figsize=(9, 3.5), constrained_layout=True)
+        ax = axes[0]
+        ax.set_title("Energy in H polarization")
+        im = ax.imshow(self.heatmap, extent=[self.Q_angles[0], self.Q_angles[-1], self.H_angles[-1], self.H_angles[0]])
+        ax.set_xlabel(r'QWP angle')
+        ax.set_ylabel(r'HWP angle')
+        fig.colorbar(im, ax=ax)
+
+        ax = axes[1]
+        ax.set_title("Cross section with most visibility")
+        I = self.heatmap.argmax()
+        i, j = np.unravel_index(I, self.heatmap.shape)
+
+        cross_section = self.heatmap[:, j]
+        ax.plot(self.H_angles, cross_section)
+        ax.set_xlabel(r'HWP angle')
+        ax.set_ylabel(r'intensity in H')
+        fig.show()
+
