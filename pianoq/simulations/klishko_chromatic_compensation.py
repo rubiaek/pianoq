b15b602cc2e964846a3eaf48fa2b4b9839ea6f4f
Ronen Shekel <shachar.resisi@mail.huji.ac.il>
2022-09-04 12:26:16 +0300

    continue work on klishko simulation

--- pianoq/simulations/klishko_chromatic_compensation.py
+++ pianoq/simulations/klishko_chromatic_compensation.py
@@ -1,13 +1,14 @@
 import pyMMF
+import pyMMF.modes
 import numpy as np
 import matplotlib.pyplot as plt
 
+N_POINTS_MODES = 2**10  # resolution of the window
 
 def get_OM1_modes(wl=0.810):
     NA = 0.275
     radius = 62.5 / 2  # in microns
     areaSize = 2.4 * radius  # calculate the field on an area larger than the diameter of the fiber
-    n_points_modes = 256  # resolution of the window
     n1 = 1.49  # index of refraction at r=0 (maximum)
     curvature = None
     k0 = 2. * np.pi / wl
@@ -23,7 +24,7 @@ def get_OM1_modes(wl=0.810):
     min_radius_bc = 1.5
 
     profile = pyMMF.IndexProfile(
-        npoints=n_points_modes,
+        npoints=N_POINTS_MODES,
         areaSize=areaSize
     )
     profile.initParabolicGRIN(n1=n1, a=radius, NA=NA)
@@ -43,6 +44,27 @@ def get_OM1_modes(wl=0.810):
                          )
 
     return modes
+
+
+def get_field(V, modes: pyMMF.modes.Modes):
+    M0 = modes.getModeMatrix()
+    field = np.zeros([N_POINTS_MODES] * 2)
+    for i in range(len(modes.betas)):
+        Mi = modes.profiles[i]
+        profile_i = Mi.reshape([N_POINTS_MODES] * 2)
+        field = field + profile_i * V[i]
+
+    return field
+
+
+def main():
+    modes810 = get_OM1_modes(wl=0.810)
+    L = 2e6  # um
+    TM = modes810.getPropagationMatrix(L)
+    V0 = np.ones(len(modes810.betas))
+    Vout = TM@V0
+
+
 
 if __name__ == "__main__":
     pass
