20cdfb613813c051135f18dc9418c4105cc9d89d
Ronen Shekel <shachar.resisi@mail.huji.ac.il>
2022-10-26 17:35:18 +0300

    better spectrum measurements, trying now also to analyze phase matching

--- pianoq/analysis/spectrum.py
+++ pianoq/analysis/spectrum.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 
 def smooth(x, N):
@@ -31,7 +32,56 @@ def show_spc_dir(path):
     plt.show()
 
 
+def gaussian(x, a, x0, sig):
+    return a*np.exp(-((x-x0)**2)/(sig**2))
+
+
+def fit_to_gaussian(x, y, show=False):
+    popt, pcov = curve_fit(gaussian, x, y, p0=(0.05, 404, 0.2)) #, bounds=([0.02, 400, 2], [1, 409, 3]))
+    perrs = np.sqrt(np.diag(pcov))
+    x0 = popt[1]
+    x0_err = perrs[1]
+
+    if show:
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.plot(x, gaussian(x, *popt), '--')
+    return x0, x0_err
+
+
+def plot_new_pump_gaussian_fit():
+    from pyspectra.readers.read_spc import read_spc
+    import glob
+    import re
+    path = r'G:\My Drive\Projects\Quantum Piano\Results\Calibrations\Spectrum\Pump2\*.spc'
+    files = glob.glob(path)
+    mws = []
+    wls = []
+    wl_errs = []
+    for f in files:
+        mw = re.findall('(.*)mW.spc', f)[0].split('\\')[-1]
+        mws.append(mw)
+        spc = read_spc(f)
+        X = np.array(spc.index)
+        Y = spc.values
+        x0, x0_err = fit_to_gaussian(X, Y, False)
+        wls.append(x0)
+        wl_errs.append(x0_err)
+
+    fig, ax = plt.subplots()
+    ax.errorbar(mws, wls, wl_errs)
+    ax.set_xlabel('pump power (mW)')
+    ax.set_ylabel('central wavelength (nm)')
+    ax.axhline(403.85, color='g', linestyle='--')
+    ax.axvline(8, color='r', linestyle='--')
+    ax.axhline(404.8, color='g', linestyle='--')
+    ax.axvline(20, color='r', linestyle='--')
+    ax.axvline(21, color='r', linestyle='--')
+    fig.show()
+
+
 if __name__ == "__main__":
+    """
     # Show best measurement of SPDC spectrum, with a wide filter (810/81), 50s integration time, and no flourecence
     # filter to gain another 30% signal
     path = r'G:\My Drive\Projects\Quantum Piano\Results\Calibrations\Spectrum\SPDC\800-80_filter_50s_no_flourescent_filter.spc'
@@ -42,6 +92,8 @@ if __name__ == "__main__":
     # Show pump spectrum at different powers
     path = r'G:\My Drive\Projects\Quantum Piano\Results\Calibrations\Spectrum\Pump'
     show_spc_dir(path)
+    """
 
+    plot_new_pump_gaussian_fit()
     plt.show()
 
