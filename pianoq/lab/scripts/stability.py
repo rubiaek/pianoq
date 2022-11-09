7864155f75ef08b03009dde99a8f2ff97e649107
Ronen Shekel <ronen.shekel@gmail.com>
2022-11-09 11:41:07 +0200

    move files around

--- pianoq/lab/scripts/stability.py
+++ pianoq/lab/scripts/stability.py
@@ -1,7 +1,9 @@
 import time
+import glob
 
 from pianoq.misc.borders import Borders
 from pianoq.lab import VimbaCamera
+from pianoq_results.image_results import load_image
 
 
 def main(interval=60):
@@ -16,6 +18,12 @@ def main(interval=60):
         time.sleep(interval)
         i += 1
         print(f'i: {(time.time()-start_time):2f}')
+
+def analyse(dir_path=rf'G:\My Drive\Projects\Quantum Piano\Results\Calibrations\Stability\*'):
+    paths = glob.glob(dir_path)
+    for path in paths:
+        im = load_image(path)
+
 
 
 if __name__ == "__main__":
