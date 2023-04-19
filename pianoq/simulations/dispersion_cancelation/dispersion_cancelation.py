6a4a203b441c5c7563fd7350eef0d1a25e094b00
rubiaek <ronen.shekel@gmail.com>
2023-04-19 12:02:18 +0300

    more simulation for dispersion project

--- pianoq/simulations/dispersion_cancelation/dispersion_cancelation.py
+++ pianoq/simulations/dispersion_cancelation/dispersion_cancelation.py
@@ -2,6 +2,7 @@ import pyMMF
 import logging
 logging.disable()
 import matplotlib.pyplot as plt
+import numpy as np
 
 SOLVER_N_POINTS_SEARCH = 2**8
 SOLVER_N_POINTS_MODE = 2**7
@@ -10,16 +11,19 @@ SOLVER_BC_RADIUS_STEP = 0.95
 SOLVER_N_BETA_COARSE = 1000
 SOLVER_MIN_RADIUS_BC = .5
 
+
 class Fiber(object):
-    def __init__(self, wl=0.808, n1=1.45, NA=0.2, diameter=50, curvature=None, areaSize=None, npoints=2**7, autosolve=True):
+    def __init__(self, wl=0.808, n1=1.45, NA=0.2, diameter=50, curvature=None, areaSize=None, npoints=2**7, autosolve=True, L=2e6):
+        """ all in um """
         self.NA = NA
         self.diameter = diameter
         self.radius = self.diameter / 2 # in microns
         self.areaSize = areaSize or 2.5*self.radius # calculate the field on an area larger than the diameter of the fiber
-        self.npoints = npoints # resolution of the window
+        self.npoints = npoints  # resolution of the window
         self.n1 = n1
-        self.wl = wl # wavelength in microns
+        self.wl = wl  # wavelength in microns
         self.curvature = curvature
+        self.L = L
 
         self.profile = pyMMF.IndexProfile(npoints=npoints, areaSize=self.areaSize)
         self.profile.initParabolicGRIN(n1=n1, a=self.radius, NA=NA)
@@ -27,6 +31,11 @@ class Fiber(object):
         self.solver = pyMMF.propagationModeSolver()
         self.solver.setIndexProfile(self.profile)
         self.solver.setWL(self.wl)
+
+        self.profile_0 = np.zeros([self.npoints]*2)
+        self.modes_0 = None
+        self.profile_end = np.zeros([self.npoints]*2)
+        self.modes_end = None
 
         self.NmodesMax = pyMMF.estimateNumModesGRIN(self.wl, self.radius, self.NA)
         self.modes = None
@@ -42,14 +51,38 @@ class Fiber(object):
         mode_repr = 'cos'  # or exp for OAM modes
 
         self.modes = self.solver.solve(mode='radial_test',
-                                       r_max=r_max, # max radius to calculate (and first try for large radial boundary condition)
-                                       dh=dh, # radial resolution during the computation
-                                       min_radius_bc=SOLVER_MIN_RADIUS_BC, # min large radial boundary condition
-                                       change_bc_radius_step=SOLVER_BC_RADIUS_STEP, #change of the large radial boundary condition if fails
-                                       N_beta_coarse=SOLVER_N_BETA_COARSE, # number of steps of the initial coarse scan
+                                       r_max=r_max,  # max radius to calculate (and first try for large radial boundary condition)
+                                       dh=dh,  # radial resolution during the computation
+                                       min_radius_bc=SOLVER_MIN_RADIUS_BC,  # min large radial boundary condition
+                                       change_bc_radius_step=SOLVER_BC_RADIUS_STEP,  # change of the large radial boundary condition if fails
+                                       N_beta_coarse=SOLVER_N_BETA_COARSE,  # number of steps of the initial coarse scan
                                        degenerate_mode=mode_repr,
                                        field_limit_tol=1e-4,)
         self.Nmodes = self.modes.number
+
+    def _get_gausian(self, sig, ravel=True):
+        """ sig in pixels """
+        X = np.arange(-self.npoints/2, self.npoints/2)
+        XX, YY = np.meshgrid(X, X)
+        # sqrt in 2d, and also 4*sig**2, because field and not power, so abs(g)**2.sum() = 1
+        g = 1 / np.sqrt(sig**2 * 2 * np.pi) * np.exp(-(XX ** 2 + YY ** 2) / (4 * sig ** 2))
+        if ravel:
+            return g.ravel()
+        else:
+            return g
+
+    def propagate(self, gaussian=True, sigma=10, shift=(3, 9)):
+        if not gaussian:
+            raise Exception('no other modes supported yet :P')
+
+        self.profile_0 = self._get_gausian(sig=sigma, ravel=True)
+        # excite modes with input of shifted gaussian
+        self.modes_0 = self.modes.getModeMatrix(shift=shift).T @ self.profile_0
+        # evolute modes
+        self.modes_end = self.modes.getPropagationMatrix(distance=self.L) @ self.modes_0
+        # modes to profile (not shifted, because it is what it is)
+        self.profile_end = self.modes_end.T @ self.modes.getModeMatrix().T
+        return self.profile_end
 
     def show_mode(self, m):
         """ m mode number"""
@@ -60,13 +93,14 @@ class Fiber(object):
 
 
 class ManyWavelengthSimulation(object):
-    def __init__(self, wl0=0.810, Dwl=0.020, N_wl=21):
+    def __init__(self, wl0=0.810, Dwl=0.020, N_wl=21, Nmodes=50):
+        """ all in um """
         self.wl0 = wl0
         self.Dwl = Dwl
         self.N_wl = N_wl
         self.wls = self._get_wl_range()
         self.fibers = [Fiber(wl=wl) for wl in self.wls]
-        self.N_modes_cutoff = self.fibers[0].Nmodes # If N_modes changes with wl - discard last modes
+        self.N_modes_cutoff = self.fibers[0].Nmodes  # If N_modes changes with wl - discard last modes
         self.betas = np.zeros((N_wl, self.N_modes_cutoff))
         self._populate_betas()
 
@@ -77,13 +111,31 @@ class ManyWavelengthSimulation(object):
     def _get_wl_range(self):
         """ in um"""
         # stolen from Logan GMMNLSE-Solver-FINAL-master\solve_for_modes.m
-        c = 299792458e6 # um/s
-        f0 = c / self.wl0 # center frequency in THz
+        c = 299792458e6  # um/s
+        f0 = c / self.wl0  # center frequency in THz
         frange = c / self.wl0**2 * self.Dwl
         df = frange / self.N_wl
         f = f0 + np.arange(-self.N_wl/2, self.N_wl/2)*df
-        l = c / f; # um
+        l = c / f  # um
         return l
+
+    def show_betas(self):
+        fig, ax = plt.subplots()
+        for f in self.fibers:
+            ax.plot(f.modes.betas, label=f'$ \lambda= ${f.wl:.4f}')
+            ax.set_xlabel('mode #')
+            ax.set_ylabel(r'Propagation constant $\beta$ (in $\mu$m$^{-1}$)')
+            ax.legend()
+        fig.show()
+
+        fig, ax = plt.subplots()
+        for i in np.arange(0, 50, 5):
+            ax.plot(self.wls, self.betas[:, i], label=f'mode no. {i}')
+            ax.set_xlabel(r'$\lambda (\mu m m)$')
+            ax.set_ylabel(r'Propagation constant $\beta$ (in $\mu$m$^{-1}$)')
+            ax.legend()
+        fig.show()
+
 
     # TODO: find length that will cause this fiber to have a spectral correlation width of ~3nm, and then check our Kilshko two-photon spectral correlation width
     # TODO: this can be defined via choosing a length L such that max_m{beta_m(w+)-beta_m(w-)}*L = 2*pi for w+ - w- = 3nm
@@ -96,8 +148,9 @@ class ManyWavelengthSimulation(object):
         # this gives Dbetas~1 at least for the first ~30 modes, except for a few modes specific modes, not sure why
         Dphis = Dbetas*L
 
-        DDbetas = s.betas[0, :] + s.betas[20, :] - 2 * s.betas[10, :] # Klyshko picture, 6nms hoping is OK
+        DDbetas = s.betas[0, :] + s.betas[20, :] - 2 * s.betas[10, :] # Klyshko picture, 20nms hoping is OK
         DDbetas -= np.median(DDbetas)
         DDphis = DDbetas*2*L  # seems good! even with 20nm bandwidth we seem to still be far off from 2pi!
+
 
 s = ManyWavelengthSimulation()
