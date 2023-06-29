d5cdc6e5bdb68625011cd61ee292bc402d466821
Ronen Shekel <shachar.resisi@mail.huji.ac.il>
2023-06-29 15:46:18 +0300

    added SPCM option in camera_contrast.py

--- pianoq/lab/scripts/camera_contrast.py
+++ pianoq/lab/scripts/camera_contrast.py
@@ -2,12 +2,15 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime
+
+from pianoq.lab.time_tagger import QPTimeTagger
 from pianoq.misc.borders import Borders
 from pianoq.lab import Edac40, ASICam
 from pianoq.lab.VimbaCamera import VimbaCamera
 from pianoq_results.QPPickleResult import QPPickleResult
 from pianoq.analysis.contrast_lib import calc_contrast, contrast_to_N_modes
 from matplotlib.patches import Rectangle
+from uncertainties import ufloat
 
 PATH = r'G:\My Drive\Projects\Dispersion Cancelation\Results\Classical'
 
@@ -19,23 +22,21 @@ class ImageList(QPPickleResult):
         self.timestamp = timestamp
         self.images = []
 
-    def contrast_from_roi_size(self, N_pixels=4):
-        im = self.images[0]
-        fig, ax = plt.subplots()
-        ax.imshow(im)
-        fig.colorbar(im, ax=ax)
-        mid_x, mid_y = im.shape[1] // 2, im.shape[0] // 2
-        ax.add_patch(Rectangle((mid_x-N_pixels, mid_y-N_pixels), 2*N_pixels, 2*N_pixels, facecolor="grey", ec='k', lw=2))
-
-        contrast = 0 # TODO: calc
-        ax.set_title(f'contrast on this area: {contrast}')
-        fig.show()
-
-    def contrast_per_pixel(self, vmin=0, vmax=1.5, binning=1):
+    def contrast_per_pixel(self, vmin=0, vmax=1.5, binning=1, remove_background=True, N=None,
+                           show_N_modes=False, N_min=0.5, N_max=20):
         images = self.images
+
+        if N is not None:
+            images = images[:N]
+
         if binning != 1:
             rows, cols = images[0].shape
             images = np.array([cv2.resize(im, (cols//binning, rows//binning), interpolation=cv2.INTER_AREA) for im in images])
+
+        if remove_background:
+            V = il.images[:, 0, 0]
+            bg = V.mean()
+            images = images.astype(float) - bg
 
         fig, ax = plt.subplots()
         mean = images.mean(axis=0)
@@ -43,19 +44,41 @@ class ImageList(QPPickleResult):
         contrast = std/mean
         imm = ax.imshow(contrast, vmin=vmin, vmax=vmax)
 
-        im = images[0]
-        mid_x, mid_y = im.shape[1] // 2, im.shape[0] // 2
-        V = [im[mid_y, mid_x] for im in images]
-        V = np.array(V)
-        sample_contrast = calc_contrast(V)
-        ax.set_title(f'sample contrast: {sample_contrast}')
+        X = 50 // binning
+        L = 100 // binning
+        ax.add_patch(Rectangle((X, X), L, L, facecolor="none", ec='k', lw=0.5))
+        mean_contrast = contrast[X:X+L, X:X+L].mean()
+        std_contrast = contrast[X:X+L, X:X+L].std()
+        c = ufloat(mean_contrast, std_contrast)
+
+        ax.set_title(f'mean contrast: {c:.3f}. Mode num: {1/c**2:.2f}')
         fig.colorbar(imm, ax=ax)
-        # print(f'contrast according to single pixel: {calc_contrast(V)}')
         fig.show()
+
+        if show_N_modes:
+            fig, ax = plt.subplots()
+            imm = ax.imshow(1/contrast**2, vmin=N_min, vmax=N_max)
+            ax.set_title(f'mean contrast: {c:.3f}. Mode num: {1/c**2:.2f}')
+            fig.colorbar(imm, ax=ax)
+            fig.show()
+
+        return contrast
 
     def loadfrom(self, path):
         super().loadfrom(path)
         self.images = np.array(self.images)
+
+
+class SingleCountsList(QPPickleResult):
+    def __init__(self, path=None, exposure_time=None, timestamp=None, **kwargs):
+        super().__init__(path=path, **kwargs)
+        self.exposure_time = exposure_time
+        self.timestamp = timestamp
+        self.single_counts = []
+
+    def loadfrom(self, path):
+        super().loadfrom(path)
+        self.single_counts = np.array(self.single_counts)
 
 
 def main(cam_type='vimba'):
@@ -68,38 +91,52 @@ def main(cam_type='vimba'):
         cam.set_exposure_time(exposure_time)
         cam.set_borders(Borders(490, 380, 700, 610))
     elif cam_type == 'ASI':
-        exposure_time = 5
-        N = 300
+        exposure_time = 0.1
+        N = 1000
 
         cam = ASICam(exposure=5, binning=2, image_bits=16, roi=(None, None, None, None))
-        cam.set_roi(1100, 830, 200, 400)
+        cam.set_roi(1100, 856, 200, 200)  # With Wollaston prism
+        # cam.set_roi(1070, 790, 200, 200)
         cam.set_exposure(exposure_time)
+    elif cam_type == 'SPCM':
+        exposure_time = 1
+        N = 1000
+        cam = QPTimeTagger(integration_time=exposure_time, coin_window=1e-9, single_channels=[2], coin_channels=[])
     else:
         raise Exception('which cam type?')
 
     dac = Edac40(max_piezo_voltage=120)
     dac.SLEEP_AFTER_SEND = 0.5
 
-    res = ImageList()
+    if cam_type == 'SPCM':
+        res = SingleCountsList()
+    else:
+        res = ImageList()
     res.exposure_time = exposure_time
     res.timestamp = timestamp
 
-    saveto_path = f"{PATH}\\{timestamp}_toptica_ASI_PBS_5s.lcam"
+    saveto_path = f"{PATH}\\{timestamp}_toptica_SPCM_PBS_1s_N=1000.lcam"
 
     try:
         for i in range(N):
             amps = np.random.rand(40)
             dac.set_amplitudes(amps)
-            im = cam.get_image()
-            res.images.append(im)
-            res.saveto(saveto_path)
-            print(f'{i}, ',end='')
+            if cam_type in ['vimba', 'PCO']:
+                im = cam.get_image()
+                res.images.append(im)
+            elif cam_type == 'SPCM':
+                s = cam.read_interesting()[0]
+                res.single_counts.append(s)
+            if i % 20 == 19:
+                res.saveto(saveto_path)
+                print(f'{i}, ',end='')
     except Exception:
         res.saveto(saveto_path)
 
+    res.saveto(saveto_path)
     cam.close()
     dac.close()
 
 
-# if __name__ == "__main__":
-#     main(cam_type='ASI')
+if __name__ == "__main__":
+    main(cam_type='SPCM')
