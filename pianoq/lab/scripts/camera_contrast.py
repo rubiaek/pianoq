b94330138c2f49b8dbaedd321c2ab90f2959d907
Ronen Shekel <shachar.resisi@mail.huji.ac.il>
2023-07-02 13:48:48 +0300

    parse saved data to output results

--- pianoq/lab/scripts/camera_contrast.py
+++ pianoq/lab/scripts/camera_contrast.py
@@ -22,12 +22,12 @@ class ImageList(QPPickleResult):
         self.timestamp = timestamp
         self.images = []
 
-    def contrast_per_pixel(self, vmin=0, vmax=1.5, binning=1, remove_background=True, N=None,
-                           show_N_modes=False, N_min=0.5, N_max=20):
+    def contrast_per_pixel(self, vmin=0, vmax=1.5, binning=1, remove_background=True, N_images=None,
+                           show_N_modes=False, N_min=0.5, N_max=20, ax=None, title=''):
         images = self.images
 
-        if N is not None:
-            images = images[:N]
+        if N_images is not None:
+            images = images[:N_images]
 
         if binning != 1:
             rows, cols = images[0].shape
@@ -38,7 +38,8 @@ class ImageList(QPPickleResult):
             bg = V.mean()
             images = images.astype(float) - bg
 
-        fig, ax = plt.subplots()
+        if ax is None:
+            fig, ax = plt.subplots()
         mean = images.mean(axis=0)
         std = images.std(axis=0)
         contrast = std/mean
@@ -51,9 +52,9 @@ class ImageList(QPPickleResult):
         std_contrast = contrast[X:X+L, X:X+L].std()
         c = ufloat(mean_contrast, std_contrast)
 
-        ax.set_title(f'mean contrast: {c:.3f}. Mode num: {1/c**2:.2f}')
-        fig.colorbar(imm, ax=ax)
-        fig.show()
+        ax.set_title(f'{title}contrast: {c:.3f}. N: {1/c**2:.2f}')
+        ax.figure.colorbar(imm, ax=ax)
+        ax.figure.show()
 
         if show_N_modes:
             fig, ax = plt.subplots()
@@ -79,6 +80,44 @@ class SingleCountsList(QPPickleResult):
     def loadfrom(self, path):
         super().loadfrom(path)
         self.single_counts = np.array(self.single_counts)
+
+
+def show_BS_PBS_Cobolt_Toptica(binning=1, suptitle='binning=2'):
+    res_toptica_PBS = ImageList()
+    res_toptica_PBS.loadfrom('G:\\My Drive\\Projects\\Dispersion Cancelation\\Results\\Classical\\2023_06_28_09_09_10_toptica_ASI_PBS_50ms_N=1000.lcam')
+    res_toptica_no_PBS = ImageList()
+    res_toptica_no_PBS.loadfrom('G:\\My Drive\\Projects\\Dispersion Cancelation\\Results\\Classical\\2023_06_28_09_43_34_toptica_ASI_no_PBS_50ms_N=1000_charmed.lcam')
+    res_cobolt_no_PBS = ImageList()
+    res_cobolt_no_PBS.loadfrom('G:\\My Drive\\Projects\\Dispersion Cancelation\\Results\\Classical\\2023_06_28_10_05_25_cobolt_ASI_no_PBS_50ms_N=1000.lcam')
+    res_cobolt_PBS = ImageList()
+    res_cobolt_PBS.loadfrom('G:\\My Drive\\Projects\\Dispersion Cancelation\\Results\\Classical\\2023_06_28_10_22_01_cobolt_ASI_PBS_100ms_N=1000.lcam')
+
+    fig, axes = plt.subplots(2, 2)
+    res_toptica_PBS.contrast_per_pixel(binning=binning, ax=axes[0, 0], title='Toptica PBS: ')
+    res_toptica_no_PBS.contrast_per_pixel(binning=binning, ax=axes[0, 1], title='Toptica no PBS: ')
+    res_cobolt_PBS.contrast_per_pixel(binning=binning, ax=axes[1, 0], title='Cobolt PBS: ')
+    res_cobolt_no_PBS.contrast_per_pixel(binning=binning, ax=axes[1, 1], title='Cobolt no PBS: ')
+    fig.suptitle(suptitle)
+    fig.show()
+
+def print_classical_SPCM_results():
+    print()
+    res_toptica_PBS = SingleCountsList()
+    res_toptica_PBS.loadfrom(r'G:\\My Drive\\Projects\\Dispersion Cancelation\\Results\\Classical\\2023_06_29_15_44_23_toptica_SPCM_PBS_1s_N=1000.lcam')
+    c = calc_contrast(res_toptica_PBS.single_counts)
+    print(f'SPCM Toptica PBS ::\t C={c:.3f}, N={1/c**2:.3f}')
+    res_toptica_no_PBS = SingleCountsList()
+    res_toptica_no_PBS.loadfrom(r'G:\\My Drive\\Projects\\Dispersion Cancelation\\Results\\Classical\\2023_06_29_14_49_02_toptica_SPCM_BS_1s_N=1000.lcam')
+    c = calc_contrast(res_toptica_no_PBS.single_counts)
+    print(f'SPCM Toptica no PBS ::\t C={c:.3f}, N={1/c**2:.3f}')
+    res_cobolt_PBS = SingleCountsList()
+    res_cobolt_PBS.loadfrom(r'G:\\My Drive\\Projects\\Dispersion Cancelation\\Results\\Classical\\2023_07_02_13_03_40_Cobolt_SPCM_PBS_1s_N=1000.lcam')
+    c = calc_contrast(res_cobolt_PBS.single_counts)
+    print(f'SPCM Cobolt PBS ::\t C={c:.3f}, N={1/c**2:.3f}')
+    res_cobolt_no_PBS = SingleCountsList()
+    res_cobolt_no_PBS.loadfrom(r"G:\My Drive\Projects\Dispersion Cancelation\Results\Classical\2023_07_02_13_36_27_Cobolt_SPCM_no_PBS_1s_N=1000.lcam")
+    c = calc_contrast(res_cobolt_no_PBS.single_counts)
+    print(f'SPCM Cobolt no PBS ::\t C={c:.3f}, N={1/c**2:.3f}')
 
 
 def main(cam_type='vimba'):
@@ -115,7 +154,7 @@ def main(cam_type='vimba'):
     res.exposure_time = exposure_time
     res.timestamp = timestamp
 
-    saveto_path = f"{PATH}\\{timestamp}_toptica_SPCM_PBS_1s_N=1000.lcam"
+    saveto_path = f"{PATH}\\{timestamp}_Cobolt_SPCM_no_PBS_1s_N=1000.lcam"
 
     try:
         for i in range(N):
