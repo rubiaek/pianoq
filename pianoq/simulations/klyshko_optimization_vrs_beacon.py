1787eb1aeeef5c434625ea8f3313fd0511adce36
rubiaek <ronen.shekel@gmail.com>
2023-09-05 15:39:48 +0300

    more Klyshko vrs. beacon optimizations simulation. Still not quite working

--- pianoq/simulations/klyshko_optimization_vrs_beacon.py
+++ pianoq/simulations/klyshko_optimization_vrs_beacon.py
@@ -4,16 +4,20 @@ from scipy.stats import unitary_group
 from pianoq.misc.mplt import *
 
 # dimension
-N = 40
+N = 64
 # TM of the thick random medium
 T = unitary_group.rvs(N)
 # The two output modes optimizing coincidences to
+I1 = 0
+I2 = 1
 O1 = N//3
 O2 = 2*N//3
 
 N_phases = 20
 
 # TODO: simply phase conjugate or something instead of searching
+# TODO: see scaling of efficiency with degree of control. Maybe Klyshko will be linear and beacon squared?
+# TODO: see scaling also with thickness / amount of memory. Simulate with help from here: https://www.nature.com/articles/nphys3373
 def optimize_beacon():
     best_cost1 = 0
     best_S1 = np.zeros(N)
@@ -71,10 +75,73 @@ def optimize_klyshko():
     return best_S, all_costs
 
 
+def get_optimal_beacon_conj(inout1=True):
+    if inout1:
+        O = O1
+        I = I1
+    else:
+        O = O2
+        I = I2
+
+    # returns angles (0, 2*pi)
+    desired_vec = np.zeros(N, complex)
+    desired_vec[O] = 1
+    at_slm = T.transpose().conjugate() @ desired_vec  # dagger
+    angles_from_out = np.angle(at_slm)
+
+    in_vec = np.zeros(N, complex)
+    in_vec[I] = 1
+    in_vec = np.fft.ifftshift(np.fft.ifft(np.fft.ifftshift(in_vec)))
+    angles_from_in = np.angle(in_vec)
+
+    S = -(angles_from_in + angles_from_out)  # TODO: this doesn't work well. Please understand it.
+
+    return S % (2*np.pi)
+
+
+def prop_beacon(S, in1=True):
+    if in1:
+        I = I1
+    else:
+        I = I2
+
+    in_vec = np.zeros(N, complex)
+    in_vec[I] = 1
+    in_vec = np.fft.ifftshift(np.fft.ifft(np.fft.ifftshift(in_vec)))
+
+    out = T @ np.diag(np.exp(1j*S)) @ in_vec
+
+    return out
+
+
+def prop_klyshko(S):
+    vec_from_O1 = np.zeros(N, complex)
+    vec_from_O1[O2] = 1  # TODO: there is something here with O1/O2 that I need to understand better
+    out = T @ np.diag(np.exp(1j*S))**2 @ T.transpose().conjugate() @ vec_from_O1
+    return out
+
+
+def get_optimal_klyshko_conj():
+    """
+        Returns phases to put on SLM (0, 2*pi)
+        Explanation:
+        (TS^2T^dag)_ij = T_ik S^2_kl T^*_jl
+        ij = O1,O2 -> T_O1k T^*_O2l S^2_kl
+        -> s_kl = (T_O1k T^*_O2l)^*
+        s digonal -> k=l
+    """
+    S = np.zeros(N, complex)
+    for k in range(N):
+        S[k] = T[O1, k].conjugate() * T[O2, k]
+
+    # dividing by 2 because we go twice on the SLM in Klyshko picture
+    return np.angle(S) / 2
+
+
 def plot_res(S, all_costs, axes=None):
     if axes is None:
         fig, axes = plt.subplots(1, 2, figsize=(9, 3))
-    final_mat = T @ np.diag(np.exp(1j*S))**2 @ T.transpose()
+    final_mat = T @ np.diag(np.exp(1j*S))**2 @ T.transpose().conjugate()
 
     X_MARKER_COLOR = '#929591'
     X_MARKER_EDGEWITDH = 1.5
@@ -95,10 +162,22 @@ def plot_res(S, all_costs, axes=None):
 
 
 if __name__ == "__main__":
+    """
+        SLM is in real basis, also the diffuser is in real basis, and also measurement. 
+        The input state should be in Fourier basis, so the SLM will do something nontrivial. 
+        
+        In Klyshko: Out1 -> U^dag -> slm -> F -> mirror -> F^-1 -> slm -> U -> Out2, so we can just forget the Fourier, 
+        everything is in real basis, so we just want to enhance the total matrix at [O1, O2]
+        
+        In beacon: I1 -> Fourier -> SLM -> U -> O1, and here we want to have only at O1. So there isn't a single index 
+        to enhance (since we have many inputs), so analytically I feel a bit stuck. I guess I want the phases at SLM 
+        plane to be the back-prop from O1, so take the phases from the backprop + phases from input, and conjugate that. 
+    """
     # S, all_costs = optimize_klyshko()
     # fig, axes = plt.subplots(1, 2, figsize=(9, 3))
     # plot_res(S, all_costs, axes)
 
-    S2, all_costs = optimize_beacon()
-    fig, axes = plt.subplots(1, 2, figsize=(9, 3))
-    plot_res(S2, all_costs, axes)
+    # S2, all_costs = optimize_beacon()
+    # fig, axes = plt.subplots(1, 2, figsize=(9, 3))
+    # plot_res(S2, all_costs, axes)
+    pass
