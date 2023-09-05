7cfcebde2091411fa3dcf682db6ee62958447647
rubiaek <ronen.shekel@gmail.com>
2023-09-05 11:17:55 +0300

    temperature piano analysis

--- pianoq/analysis/temperature_piano.py
+++ pianoq/analysis/temperature_piano.py
@@ -1,42 +1,53 @@
 import os
+import re
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
 from pianoq_results.image_result import VimbaImage
 
 
-paths = glob.glob(r'G:\My Drive\Projects\Quantum Piano\Results\TemperaturePiano\try1\*.cam')
+paths = sorted(glob.glob(r'G:\My Drive\Projects\Quantum Piano\Results\TemperaturePiano\try2\*.cam'))
 ims = [VimbaImage(path) for path in paths]
+mask = np.index_exp[90:240, 100:250]
+Ts = []
+for im in ims:
+    T = re.findall('.*T=(.*)C.*', im.path)[0]
+    Ts.append(float(T))
 
-def get_correlation(im1, im2, use_mask=False):
-    if use_mask:
-        mask = get_correlations_mask()
-        im1 = im1[mask]
-        im2 = im2[mask]
 
+def get_correlation(im1, im2):
     numerator = np.sum((im1 - np.mean(im1)) * (im2 - np.mean(im2)))
-
     A = (im1 - np.mean(im1))**2
     B = (im2 - np.mean(im2))**2
     denumerator = np.sqrt(np.sum(A) * np.sum(B))
     dist_ncc = numerator / denumerator
     return dist_ncc
 
-mask = np.index_exp[90:240, 100:250]
-corrs = []
-Ts = []
-for im in ims:
-    corr = get_correlation(ims[0].image[mask], im.image[mask])
-    corrs.append(corr)
-    T = os.path.basename(im.path)[20:]
-    Ts.append(T)
-    print(f'{T} :: {corr:.3f}')
 
-fig, axes = plt.subplots(2, 3, figsize=(12, 6), constrained_layout=True)
-for i, ax in enumerate(axes.flatten()):
-    imm = ax.imshow(ims[i].image[mask])
-    ax.set_title(f'{Ts[i]} :: {corrs[i]:.3f}')
-    fig.colorbar(imm, ax=ax)
-fig.show()
+def plot_correlations(ref_index=1):
+    corrs = []
+    for im in ims:
+        corr = get_correlation(ims[ref_index].image[mask], im.image[mask])
+        corrs.append(corr)
+    fig, ax = plt.subplots()
+    ax.plot(Ts, corrs, '*')
+    ax.set_xlabel('Temperature')
+    ax.set_ylabel('PCC')
+    ax.plot(Ts[ref_index], corrs[ref_index], 'o')
+    fig.show()
+
+
+def plot_reps(indexes=(1, 2, 3, 4, 5, 6), ref_index=1):
+    corrs = []
+    for ind in indexes:
+        corr = get_correlation(ims[ref_index].image[mask], ims[ind].image[mask])
+        corrs.append(corr)
+
+    fig, axes = plt.subplots(2, 3, figsize=(12, 6), constrained_layout=True)
+    for i in range(len(indexes)):
+        imm = axes.flatten()[i].imshow(ims[indexes[i]].image[mask])
+        axes.flatten()[i].set_title(f'{Ts[indexes[i]]} :: {corrs[i]:.3f}')
+        fig.colorbar(imm, ax=axes.flatten()[i])
+    fig.show()
 
 plt.show()
