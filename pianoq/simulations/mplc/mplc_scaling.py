51292bc151a4bc8bc475b22b2be58ee22e880832
rubiaek <ronen.shekel@gmail.com>
2024-06-18 11:59:19 +0300

    began MPLC scaling code

--- pianoq/simulations/mplc/mplc_scaling.py
+++ pianoq/simulations/mplc/mplc_scaling.py
@@ -1,17 +1,37 @@
+import numpy as np
 from pianoq.simulations.mplc.mplc_result import MPLCResult
+from pianoq.simulations.mplc.mplc import MPLC
+from pianoq.simulations.mplc.mplc_modes import get_spot_conf
+from pianoq.simulations.mplc.mplc_utils import show_field
 
 res = MPLCResult()
-res.loadfrom("C:\\temp\\speckle_speckle.mplc")
+res.loadfrom("C:\\temp\\speckle_speckle3.mplc")
+
+mplc = MPLC(conf=res.conf)
+spot = get_spot_conf(res.conf, sig=0.1, Dx0=-0.3, Dy0=0.2)
+show_field(spot, active_slice=res.active_slice)
+spot_power = ((np.abs(spot)**2)[res.active_slice]).sum()
+print(f'{spot_power=}')
+
+mplc.res.masks = res.masks
+E_out = mplc.propagate_mplc(initial_field=spot)
+show_field(E_out, active_slice=res.active_slice)
+forward_speckle_power = ((np.abs(E_out)**2)[res.active_slice]).sum()
+print(f'{forward_speckle_power=}')
+
+
+mplc.res.masks = res.masks[::-1]
+E_out2 = mplc.propagate_mplc(initial_field=spot)
+show_field(E_out2, active_slice=res.active_slice)
+backward_speckle_power = ((np.abs(E_out2)**2)[res.active_slice]).sum()
+print(f'{backward_speckle_power=}')
+
 
 # TODO: Plan
-#  Show what happens to a spot through the calculated MPLC
 #  Show this forward & backward & also Klyshko (==both)
 #  Implement SLM for focusing in relevant region (Start with SLM1 in backprop)
 #  Probably will need some statistics with different MPLC phase realizations
 #  Scale incomplete control
 #  Practically:
-#  * implement `propagate_MPLC(initial_field=E, backward=True)`, which will need a `freespace(E, L)` helper function
-#   - note that "backwards" has two meanings: 1) exp(-1*j), 2) start from plane N-1.
-#   - do this in current MPLC class. maybe `propagate_MPLC(initial_field=E, start_plane=N-1, end_plane=6)`
-#  * implement `find phase` which should be pretty easy
+#  * implement SLM WFS `find phase` which should be pretty easy
 #  * implement some incomplete control mechanism
