c2e69a5372fe248d295dd2bc5f0c3159d51e3b56
Ronen Shekel <ronen.shekel@mail.huji.ac.il>
2024-07-24 17:34:14 +0300

    begin connecting tosetup

--- pianoq/lab/mplc/consts.py
+++ pianoq/lab/mplc/consts.py
@@ -3,3 +3,10 @@ SLM_DIMS = (1024, 1280)
 MASK_DIMS = (140, 360)
 
 MASK_CENTERS = [(), (), ()]
+
+thorlabs_x_serial = 26003414 # stepper
+thorlabs_y_serial = 27501989 # DC
+
+# TODO: check XY
+# x = ThorlabsKcubeDC(27501989)
+# y = ThorlabsKcubeStepper(26003414)
