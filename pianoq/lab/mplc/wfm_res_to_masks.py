3f368fd229af4279e4186877fd7dcdc83b3ba8db
rubiaek <ronen.shekel@gmail.com>
2024-08-05 09:36:34 +0300

    all works up to a single pixel with diff of 255, where value is 213, but I really don't care enough for this

--- pianoq/lab/mplc/wfm_res_to_masks.py
+++ pianoq/lab/mplc/wfm_res_to_masks.py
@@ -8,9 +8,50 @@ from pianoq.lab.mplc.consts import PIXEL_SIZE, N_SPOTS, SPOT_WAIST_IN
 ORIG_MASKS_PATH = r"G:\My Drive\Ohad and Giora\MPLC\matlab codes\Ronen stuff 17.7.24\Masks_31_10_23_QKD5d_MUB2_mm_33_3_conjbases.mat"
 ORIG_PHASES_PATH = r"G:\My Drive\Ohad and Giora\MPLC\matlab codes\Ronen stuff 17.7.24\phase_align_QKD5d_10_11_23_3.mat"
 
+"""
+modes are ordered like this:
+25 20 15 10 5 
+24 19 14 9  4 
+23 18 13 8  3 
+22 17 12 7  2 
+21 16 11 6  1 
 
-def matlab_WFM_masks_to_ronen_masks(out_path, orig_masks_path=ORIG_MASKS_PATH, orig_phases_path=ORIG_PHASES_PATH):
+26 31 36 41 46
+27 32 37 42 47
+28 33 35 43 48
+29 34 39 44 49
+30 35 40 45 50 
+"""
 
+
+def remove_input_modes(MASKS, modes_to_keep):
+    masks = MASKS.copy()
+    XX, YY = make_grid_phasmask()
+    x_modes_in, y_modes_in = calc_pos_modes_in()
+
+    lin_mask = np.exp(-2j*np.pi*XX/(8*PIXEL_SIZE))  # 2 pi within 8 pixels
+
+    for k in range(2 * N_SPOTS):
+        if k + 1 not in modes_to_keep:  # Python uses 0-based indexing, so we add 1 to match MATLAB's 1-based indexing
+            condition = np.sqrt((XX - x_modes_in[k])**2 + (YY - y_modes_in[k])**2) < (2 * SPOT_WAIST_IN)
+            masks[0][condition] = lin_mask[condition]
+
+    return masks
+
+
+def add_phase_input_spots(MASKS, phases_path):
+    masks = MASKS.copy()
+    XX, YY = make_grid_phasmask()
+    x_modes_in, y_modes_in = calc_pos_modes_in()
+    phases = np.squeeze(scipy.io.loadmat(phases_path)['phases'])
+    for k in range(2 * N_SPOTS):
+        condition = np.sqrt((XX - x_modes_in[k])**2 + (YY - y_modes_in[k])**2) < (2 * SPOT_WAIST_IN)
+        masks[0][condition] = masks[0][condition] * np.exp(1j*phases[k])
+
+    return masks
+
+
+def matlab_WFM_masks_to_masks(out_path, orig_masks_path=ORIG_MASKS_PATH, orig_phases_path=ORIG_PHASES_PATH):
     MASKS = scipy.io.loadmat(orig_masks_path)['MASKS'].astype(complex)
     # This is the size_factor which is basically always 3 (3*MASK_DIMS)
     height, width = (1080, 420)
@@ -22,11 +63,6 @@ def matlab_WFM_masks_to_ronen_masks(out_path, orig_masks_path=ORIG_MASKS_PATH, o
 
     # Matlab "cut_masks" - carves out the middle, and takes only first 10 masks
     MASKS = MASKS[:10, 360:720, 140:280]
-    MASKS = np.angle(MASKS).astype(float)
-
-    # Matlab `remove_input_modes`
-    XX, YY = make_grid_phasmask()
-    x_modes_in, y_modes_in = calc_pos_modes_in()
 
     modes_to_keep = np.array([1, 6, 11, 16, 21, 26, 31, 36, 41, 46])
     row_num_signal = 3
@@ -34,20 +70,10 @@ def matlab_WFM_masks_to_ronen_masks(out_path, orig_masks_path=ORIG_MASKS_PATH, o
     modes_to_keep[:5] += (row_num_signal - 1)
     modes_to_keep[5:] += (row_num_idler - 1)
 
-    lin_mask = -2*np.pi*XX/(8*PIXEL_SIZE)  # 2 pi within 8 pixels
+    MASKS = remove_input_modes(MASKS, modes_to_keep=modes_to_keep)
+    MASKS = add_phase_input_spots(MASKS, orig_phases_path)
 
-    for k in range(2 * N_SPOTS):
-        if k + 1 not in modes_to_keep:  # Python uses 0-based indexing, so we add 1 to match MATLAB's 1-based indexing
-            condition = np.sqrt((XX - x_modes_in[k])**2 + (YY - y_modes_in[k])**2) < (2 * SPOT_WAIST_IN)
-            MASKS[0][condition] = lin_mask[condition]
-
-    # Matlab `add_phase_input_spots`
-    # phases = np.zeros(50)
-    phases = np.squeeze(scipy.io.loadmat(orig_phases_path)['phases'])
-    for k in range(2 * N_SPOTS):
-        condition = np.sqrt((XX - x_modes_in[k])**2 + (YY - y_modes_in[k])**2) < (2 * SPOT_WAIST_IN)
-        MASKS[0][condition] = MASKS[0][condition] + phases[k]
-
+    MASKS = np.angle(MASKS).astype(float)
     f = open(out_path, 'wb')
     np.savez(f, masks=MASKS)
     f.close()
