added fixing of initial phases for spots + fidelity calculation.

pianoq/simulations/mplc_sim/mplc_sim.py
@@ -72,7 +72,10 @@ class MPLCSim:
         self.res.forward_fields[0, :, :, :] = input_modes
         self.res.backward_fields[self.N_planes - 1, :, :, :] = output_modes
 
-    def find_phases(self, iterations=None, show_mean_fidelities=True):
+    def find_phases(self, iterations=None, show_fidelities=True, fix_initial_phases=True):
+        """
+            Fix initial phases is relevant only when input modes are spatially separated spots
+        """
         # Running with iterations = 1 will result with only field initialization
         iterations = iterations or self.N_iterations
         for iter_no in tqdm(range(iterations)):
@@ -110,41 +113,33 @@ class MPLCSim:
                                                                                     # after plane i-1
                                                                                     self.dist_after_plane[plane_no-1],
                                                                                     backprop=True)
-            if show_mean_fidelities:
+            if show_fidelities:
                 self.res._calc_fidelity()
-                ff = np.abs(np.diag(self.res.forward_fidelity)).mean()
-                bf = np.abs(np.diag(self.res.backward_fidelity)).mean()
-                self.log(f'F-fidelity: {ff}. B-fidelity: {bf}.')
+                self.log(f'{iter_no}. Fidelity: {self.res.fidelity}')
+
+        if fix_initial_phases:
+            self.fix_initial_phases()
+            # Forward pass again with fixed phases
+            for plane_no in range(self.N_planes - 1):
+                for mode_no in range(self.N_modes):
+                    E = np.copy(self.res.forward_fields[plane_no, mode_no, :, :])
+                    E *= np.exp(+1j * np.angle(self.res.masks[plane_no]))
+                    self.res.forward_fields[plane_no + 1, mode_no, :, :] = self.prop(E,
+                                                                                     self.dist_after_plane[plane_no],
+                                                                                     backprop=False)
 
     def fix_initial_phases(self):
-        pass
-        # TODO: using Neta's idea something like this in comment
-    """
-        M = Calc_Matrix(MODES, np.squeeze(FIELDS_pc[0, -1, :, :, :]))  # Using Neta's idea for calibrating the phases at the input
-        phases = np.angle(np.diag(M))
-        Mask_phase_cal = np.zeros_like(X)
+        self.res._calc_normalized_overlap()
+        phases = np.angle(np.diag(self.res.forward_overlap))
+        mask_phase_cal = np.zeros_like(self.XX)
 
         for l in range(len(phases)):
-            Mask_phase_cal[np.sqrt((X - x_modes_in[l])**2 + (Y - y_modes_in[l])**2) < (2 * waist_in)] = -phases[l]
-        
-        MASKS[0] = np.exp(1j * (np.angle(np.squeeze(MASKS[0])) + Mask_phase_cal))  # calibrate global phase between the terms using the first mask
-        
-        # Function definition
-        def Calc_Matrix(MODES, Output_fields):
-            M = np.zeros((MODES.shape[0], Output_fields.shape[0]), dtype=complex)
-            
-            for j in range(MODES.shape[0]):
-                mode = np.squeeze(MODES[j])
-                mode = mode / np.sqrt(np.sum(np.abs(mode)**2))
-                
-                for k in range(Output_fields.shape[0]):
-                    output1 = np.squeeze(Output_fields[k])
-                    output1 = output1 / np.sqrt(np.sum(np.abs(output1)**2))
-                    M[j, k] = np.sum(output1 * np.conj(mode))
-            
-            return M
+            spot = np.abs(self.res.forward_fields[0, l])
+            mask_phase_cal[spot > spot.max() / np.e ** 2] = -phases[l]
 
-    """
+        # calibrate global phase between the terms using the first mask
+        self.res.masks[0] = np.exp(1j * (np.angle(np.squeeze(self.res.masks[0])) + mask_phase_cal))
+        
 
     def update_mask(self, plane_no):
         # some planes have constant phase mask (lens, next to lens, etc.)

pianoq/simulations/mplc_sim/mplc_sim_result.py
@@ -16,27 +16,47 @@ class MPLCSimResult:
         self.__dict__.update(self.conf)
         self.N_planes = len(self.conf.get('active_planes', ()))
 
-        self.forward_fidelity = np.array([], dtype=np.complex64)
-        self.backward_fidelity = np.array([], dtype=np.complex64)
+        self.forward_overlap = np.array([], dtype=np.complex64)
+        self.backward_overlap = np.array([], dtype=np.complex64)
         self.forward_losses = np.zeros(self.N_modes)
         self.backward_losses = np.zeros(self.N_modes)
+        self.fidelity = 0
 
     def _calc_fidelity(self):
-        self.forward_fidelity = np.zeros((self.N_modes, self.N_modes), dtype=np.complex64)
-        self.backward_fidelity = np.zeros((self.N_modes, self.N_modes), dtype=np.complex64)
+        self._calc_normalized_overlap()
+        M = self.forward_overlap
+        U = np.eye(self.N_modes)
+        numerator = abs(np.trace(np.dot(U, np.conj(M).T)))
+        denominator = np.sqrt(abs(np.trace(np.dot(U, np.conj(U).T)) * np.trace(np.dot(M, np.conj(M).T))))
+        self.fidelity = numerator / denominator
+
+    def _calc_normalized_overlap(self):
+        self.forward_overlap = np.zeros((self.N_modes, self.N_modes), dtype=np.complex64)
+        self.backward_overlap = np.zeros((self.N_modes, self.N_modes), dtype=np.complex64)
 
         # TODO: have a finer dx for reality grid, with each SLM pixel being 2X2 reality pixels etc.?
         for in_mode in range(self.N_modes):
             for out_mode in range(self.N_modes):
-                forward_fidelity = (self.forward_fields[-1, in_mode] *
-                                    self.backward_fields[-1, out_mode].conj()
-                                    ).sum()
-                self.forward_fidelity[in_mode, out_mode] = forward_fidelity
+                desired_field = self.backward_fields[-1, out_mode]
+                expected_field = self.forward_fields[-1, in_mode]
 
-                backward_fidelity = (self.backward_fields[0, in_mode] *
-                                     self.forward_fields[0, out_mode].conj()
-                                     ).sum()
-                self.backward_fidelity[in_mode, out_mode] = backward_fidelity
+                # Normalize
+                desired_field = desired_field / np.sqrt(np.sum(np.abs(desired_field) ** 2))
+                expected_field = expected_field / np.sqrt(np.sum(np.abs(expected_field) ** 2))
+
+                forward_overlap = (expected_field * desired_field.conj()).sum()
+                self.forward_overlap[in_mode, out_mode] = forward_overlap
+
+                desired_field = self.backward_fields[0, in_mode]
+                expected_field = self.forward_fields[0, out_mode]
+
+                # Normalize
+                desired_field = desired_field / np.sqrt(np.sum(np.abs(desired_field) ** 2))
+                expected_field = expected_field / np.sqrt(np.sum(np.abs(expected_field) ** 2))
+
+                backward_overlap = (expected_field * desired_field.conj()).sum()
+                self.backward_overlap[in_mode, out_mode] = backward_overlap
+
 
     def _calc_loss(self):
         # TODO: check this
@@ -48,27 +68,27 @@ class MPLCSimResult:
         for mode_no in range(self.N_modes):
             backward_losses[mode_no] = (np.abs(self.backward_fields[0, mode_no]) ** 2)[self.active_slice].sum()
 
-    def show_fidelity(self):
-        self._calc_fidelity()
+    def show_overlap(self):
+        self._calc_normalized_overlap()
         fig, axes = plt.subplots(1, 2, constrained_layout=True)
-        imm = axes[0].imshow(np.abs(self.forward_fidelity))
+        imm = axes[0].imshow(np.abs(self.forward_overlap))
         fig.colorbar(imm, ax=axes[0])
-        axes[0].set_title('Forward fidelity')
+        axes[0].set_title('Forward overlap')
         axes[0].set_ylabel('Input modes')
         axes[0].set_xlabel('Output modes')
 
-        for i in range(self.forward_fidelity.shape[0]):
-            for j in range(self.forward_fidelity.shape[1]):
-                axes[0].text(j, i, f'{np.abs(self.forward_fidelity[i, j]):.2f}', ha='center', va='center', color='white')
+        for i in range(self.forward_overlap.shape[0]):
+            for j in range(self.forward_overlap.shape[1]):
+                axes[0].text(j, i, f'{np.abs(self.forward_overlap[i, j]):.2f}', ha='center', va='center', color='white')
 
-        imm = axes[1].imshow(np.abs(self.backward_fidelity))
+        imm = axes[1].imshow(np.abs(self.backward_overlap))
         fig.colorbar(imm, ax=axes[1])
-        axes[1].set_title('Backward fidelity')
+        axes[1].set_title('Backward overlap')
         axes[1].set_xlabel('Input modes')
         axes[1].set_ylabel('Output modes')
-        for i in range(self.backward_fidelity.shape[0]):
-            for j in range(self.backward_fidelity.shape[1]):
-                axes[1].text(j, i, f'{np.abs(self.backward_fidelity[i, j]):.2f}', ha='center', va='center', color='white')
+        for i in range(self.backward_overlap.shape[0]):
+            for j in range(self.backward_overlap.shape[1]):
+                axes[1].text(j, i, f'{np.abs(self.backward_overlap[i, j]):.2f}', ha='center', va='center', color='white')
 
         fig.show()
 