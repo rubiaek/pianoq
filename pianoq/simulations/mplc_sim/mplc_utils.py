959eda64959be7bbec54f1806d840d3f701421c7
rubiaek <ronen.shekel@gmail.com>
2024-09-05 10:29:06 +0300

    better weighted average for MPLC ncomplete control

--- pianoq/simulations/mplc_sim/mplc_utils.py
+++ pianoq/simulations/mplc_sim/mplc_utils.py
@@ -51,52 +51,35 @@ def show_mask(mask, figshow=True, active_slice=None, title=''):
         fig.show()
 
 
-def downsample_phase(A, L, weighted=True):
+def downsample_phase(E, L, weighted=True):
     """ A is a complex field.
         Returns the down-sampled phase of A (in an exponent)"""
 
-    N, M = A.shape
-    phase = np.angle(A)
-    amplitude = np.abs(A)
-
     if L == 1:
-        return np.exp(1j * phase)
+        return np.exp(1j * np.angle(E))
 
+    N, M = E.shape
     # Pad arrays if necessary
     pad_n, pad_m = (L - N % L) % L, (L - M % L) % L
-    phase_padded = np.pad(phase, ((0, pad_n), (0, pad_m)), mode='constant')  # default constant_values is 0
-    amplitude_padded = np.pad(amplitude, ((0, pad_n), (0, pad_m)), mode='constant')  # the zero amplitude makes sure it won't affect the weighted mean
+    # default constant_values is 0, the zero amplitude makes sure it won't affect the weighted mean
+    padded_E = np.pad(E, ((0, pad_n), (0, pad_m)), mode='constant')
+    reshaped_E = padded_E.reshape((N + pad_n) // L, L, (M + pad_m) // L, L)
 
-    # Reshape and compute weighted mean
-    phase_reshaped = phase_padded.reshape((N + pad_n) // L, L, (M + pad_m) // L, L)
-    amplitude_reshaped = amplitude_padded.reshape((N + pad_n) // L, L, (M + pad_m) // L, L)
-
-    weights = amplitude_reshaped if weighted else None
-
-    if not weighted:
-        weighted_phase = np.average(phase_reshaped, axis=(1, 3))
+    if weighted:
+        # angle of (Sum of complex numbers) will give the weighted sum, or rather the angle of the summed phasor
+        summed_E = np.sum(reshaped_E, axis=(1, 3))
+        summed_phase = np.angle(summed_E)
+        # Upsample the result back to NxM
+        result = np.repeat(np.repeat(summed_phase, L, axis=0), L, axis=1)
     else:
-        weight_sum = np.sum(weights, axis=(1, 3))
-        zero_weight_mask = weight_sum == 0  # Create a mask for areas where the sum of weights is zero (some areas have zero amplitude)
-
-        # Calculate the weighted average, but only where weights are non-zero
-        weighted_phase = np.where(
-            zero_weight_mask,
-            np.nan,  # Temporarily assign NaN where weights are zero
-            # TODO: this doesn't work.
-            np.average(phase_reshaped, weights=weights, axis=(1, 3))
-        )
-
-        # Replace NaN values with the regular average
-        if np.any(zero_weight_mask):
-            regular_avg = np.average(phase_reshaped, axis=(1, 3))
-            weighted_phase = np.where(zero_weight_mask, regular_avg, weighted_phase)
-
-    # Upsample the result back to NxM
-    result = np.repeat(np.repeat(weighted_phase, L, axis=0), L, axis=1)
+        phase_reshaped = np.angle(reshaped_E)
+        weighted_phase = np.average(phase_reshaped, axis=(1, 3))
+        # Upsample the result back to NxM
+        result = np.repeat(np.repeat(weighted_phase, L, axis=0), L, axis=1)
 
     # Trim to original size
-    return np.exp(1j*result[:N, :M])
+    return np.exp(1j * result[:N, :M])
+
 
 def corr(A, B):
     return np.abs((A.conj() * B).sum())
