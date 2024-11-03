bad3365a615635ad5c901b23d285be467a279fd6
rubiaek <ronen.shekel@gmail.com>
2024-11-03 15:01:38 +0200

    SLM3 nonlinear simulation

--- pianoq/simulations/abstract_quantum_scaling/slm3_simulation.py
+++ pianoq/simulations/abstract_quantum_scaling/slm3_simulation.py
@@ -1,38 +1,55 @@
 import numpy as np
+import random
+from scipy.stats import unitary_group
 from scipy.optimize import minimize, dual_annealing
 
-
 class SLM3Simulation:
-    def __init__(self):
-        self.N = 256
+    def __init__(self, N=256, T_method='gaus_iid'):
+        self.N = N
+        self.T_method = T_method
         self.T = self.get_diffuser()
-        self.v_in = 1/np.sqrt(self.N) * np.ones
-        self.slm_phases = np.zeros(self.N)
+
+        self.v_in = 1/np.sqrt(self.N) * np.ones(self.N, dtype=np.complex128)
+        self.slm_phases = np.exp(1j*np.zeros(self.N))
+        self.f_calls = 0
 
     def get_diffuser(self):
-        return 1/np.sqrt(self.N) * np.random.normal(loc=0, scale=np.sqrt(2)/2, size=(self.N, self.N, 2)).view(np.complex128)[:, :, 0]
+        if self.T_method == 'gaus_iid':
+            return 1/np.sqrt(self.N) * np.random.normal(loc=0, scale=np.sqrt(2)/2, size=(self.N, self.N, 2)).view(np.complex128)[:, :, 0]
+        elif self.T_method == 'unitary':
+            return unitary_group.rvs(self.N)
+        else:
+            raise NotImplementedError()
+
+    def reset_T(self):
+        self.T = self.get_diffuser()
 
     def propagate(self):
         # v_in gets slm phases, is scattered by T, and is reflected by the crystal,
         # then T.T, phases againg, and the FFT for phases to take effect
 
-        v_out = np.fft.fft(self.slm_phases * (self.T.transpose() @ self.T @ (self.slm_phases * self.v_in)))
+        after_SLM_second_time = self.slm_phases * (self.T.transpose() @ self.T @ (self.slm_phases * self.v_in))
+        v_out = np.fft.fft(after_SLM_second_time) / np.sqrt(self.N)
         return v_out
 
-    def get_intensity(self, slm_phases, out_mode=None):
-        self.slm_phases = slm_phases
+    def get_intensity(self, slm_phases=None, out_mode=None):
+        if slm_phases is not None:
+            self.slm_phases = np.exp(1j*slm_phases)
         out_mode = out_mode or self.N // 2
         v_out = self.propagate()
         I = np.abs(v_out[out_mode])**2
+        self.f_calls += 1
         return -I
 
-    def optimize(self, method="gradient_descent"):
+    def optimize(self, method="slsqp"):
+        import numpy as np # really weird, don't understand why I need this
+        self.f_calls = 0
         # Define initial phases as the current slm_phases
-        initial_phases = self.slm_phases.copy()
+        initial_phases = np.exp(1j*np.zeros(self.N))
 
-        if method == "gradient_descent":
+        if method == "slsqp" or method == "L-BFGS-B":
             result = minimize(
-                self.get_intensity, initial_phases, method="L-BFGS-B", bounds=[(0, 2 * np.pi)] * self.N
+                self.get_intensity, initial_phases, method=method, bounds=[(0, 2 * np.pi)] * self.N
             )
             self.slm_phases = result.x
             intensity = self.get_intensity(self.slm_phases)
@@ -42,13 +59,21 @@ class SLM3Simulation:
             bounds = [(0, 2 * np.pi) for _ in range(self.N)]
             result = dual_annealing(self.get_intensity, bounds=bounds)
             self.slm_phases = result.x
+            intensity = self.get_intensity(self.slm_phases)
+            return intensity, result
 
         elif method == "genetic_algorithm":
             from deap import base, creator, tools, algorithms
 
-            # Create necessary classes for DEAP genetic algorithm
-            creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-            creator.create("Individual", list, fitness=creator.FitnessMax)
+            from deap import base, creator, tools, algorithms
+            import random
+            import numpy as np
+
+            # Check if FitnessMax and Individual classes already exist to avoid redefinition warnings
+            if not hasattr(creator, "FitnessMax"):
+                creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+            if not hasattr(creator, "Individual"):
+                creator.create("Individual", list, fitness=creator.FitnessMax)
 
             # Initialize DEAP toolbox
             toolbox = base.Toolbox()
@@ -62,13 +87,17 @@ class SLM3Simulation:
 
             # Genetic algorithm parameters
             population = toolbox.population(n=100)
-            algorithms.eaSimple(population, toolbox, cxpb=0.7, mutpb=0.2, ngen=40, verbose=False)
+            max_generations = 2000  # Set this to your desired number of generations
+            algorithms.eaSimple(population, toolbox, cxpb=0.7, mutpb=0.2, ngen=max_generations, verbose=False)
+
             best_individual = tools.selBest(population, k=1)[0]
 
             # Update slm_phases with best individual found
             self.slm_phases = np.array(best_individual)
+            intensity = self.get_intensity(self.slm_phases)
+            return intensity, population
 
-        elif method == "particle_swarm":
+        elif method == "PSO":
             try:
                 from pyswarm import pso
             except ImportError:
@@ -76,8 +105,10 @@ class SLM3Simulation:
 
             lb = [0] * self.N
             ub = [2 * np.pi] * self.N
-            best_phases, _ = pso(self.get_intensity, lb, ub)
+            best_phases, _ = pso(self.get_intensity, lb, ub, maxiter=150000)
             self.slm_phases = best_phases
+            intensity = self.get_intensity(self.slm_phases)
+            return intensity, _
 
         else:
             raise ValueError("Unsupported optimization method")
